e8f458f67f693c2221393f612c00daa2f7ed66f9
Soma Ishihara <221008@st.yoshida-g.ac.jp>
2023-01-12 09:28:49 +0900

    テストプログラム完成

---
+++ debugproc.h
@@ -0,0 +1,16 @@
+//==========================================
+//
+//デバッグ表示プログラムのヘッダ[debugproc.h]
+//Author:石原颯馬
+//
+//==========================================
+#ifndef _DEBUGPROC_H_
+#define _DEBUGPROC_H_
+
+//プロトタイプ宣言
+void InitDebugProc(void);
+void UninitDebugProc(void);
+void DrawDebugProc(void);
+void PrintDebugProc(const char *fmt, ...);
+
+#endif // !_DEBUGPROC_H_
